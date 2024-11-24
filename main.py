1498e6f846c7090d37e280e37b1f42ae23494296
gleb.parfenov <gleb.parfenov@tabby.ai>
2024-11-25 00:41:52 +0100

    draw the player

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    pygame.init()
     running = True
 
-    pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    cl = pygame.time.Clock()
+    player = Player(SCREEN_HEIGHT / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill((0, 0, 0))
+    
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        dt = cl.tick(60) / 1000
 
 if __name__ == "__main__":
     main()    
